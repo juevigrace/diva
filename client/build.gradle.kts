db5b9fa27c65152d5b183dae1d0a8d6900015d72
juevigrace <juev1grace@gmail.com>
2026-08-27 13:44:20 -0400

    add wrappers for each project

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 val cleanAll = tasks.register<Delete>("cleanAll") {
     group = "build"
-    description = "Deletes all build/ and .gradle/ directories across the composite builds."
+    description = "Deletes all build/ directories across the composite builds."
     val rootDir = rootProject.projectDir
     delete(
         rootDir
             .walkTopDown()
-            .filter { it.isDirectory && (it.name == "build" || it.name == ".gradle") }
+            .filter { it.isDirectory && it.name == "build" }
             .toList(),
     )
 }
