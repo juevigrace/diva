c634a273a40895ed638f824c9d214e9f6e1833e3
juevigrace <juev1grace@gmail.com>
2026-09-25 09:26:57 -0400

    add dependency substitution map

--- client/apps/diva/settings.gradle.kts
+++ client/apps/diva/settings.gradle.kts
@@ -88,4 +88,36 @@ include(":features:server-database")
 project(":features:server-database").projectDir = file("features/server/database")
 
 includeBuild("../../packages/shared/framework")
-includeBuild("../../packages/shared/app-lib")
+
+includeBuild("../../packages/shared/app-lib") {
+    dependencySubstitution {
+        val appLibModules =
+            mapOf(
+                "diva-lib-core" to ":core",
+                "diva-lib-core-models" to ":core-models",
+                "diva-lib-database" to ":database",
+                "diva-lib-ui" to ":ui",
+                "diva-lib-auth" to ":features:auth:auth-core",
+                "diva-lib-auth-models" to ":features:auth:auth-models",
+                "diva-lib-devices" to ":features:devices:devices-core",
+                "diva-lib-devices-database" to ":features:devices:devices-database",
+                "diva-lib-devices-models" to ":features:devices:devices-models",
+                "diva-lib-onboarding" to ":features:onboarding:onboarding-core",
+                "diva-lib-permissions" to ":features:permissions:permissions-core",
+                "diva-lib-permissions-database" to ":features:permissions:permissions-database",
+                "diva-lib-permissions-models" to ":features:permissions:permissions-models",
+                "diva-lib-session" to ":features:session:session-core",
+                "diva-lib-session-database" to ":features:session:session-database",
+                "diva-lib-session-models" to ":features:session:session-models",
+                "diva-lib-settings" to ":features:settings:settings-core",
+                "diva-lib-user" to ":features:user:user-core",
+                "diva-lib-user-database" to ":features:user:user-database",
+                "diva-lib-user-models" to ":features:user:user-models",
+                "diva-lib-verification" to ":features:verification:verification-core",
+                "diva-lib-verification-models" to ":features:verification:verification-models",
+            )
+        appLibModules.forEach { (moduleName, projectPath) ->
+            substitute(module("io.github.juevigrace:$moduleName")).using(project(projectPath))
+        }
+    }
+}
